15bd0be9156ca8738b63da165c09fbf2ab248423
Fetozo <fetozo96@gmail.com>
2024-04-10 10:37:36 +0300

    Добавьте файлы проекта.

---
+++ Controllers/AccountController.cs
@@ -0,0 +1,92 @@
+﻿//using System.Web.UI;
+//using System.Web.UI.WebControls;
+//using System.Web.Security.FormsAuthentication;
+using HelpDeskTrain.Models;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System.Net;
+using System.Security.Claims;
+//using System.Web.UI.Page;
+
+namespace HelpDeskTrain.Controllers
+{
+    [AllowAnonymous]
+    public class AccountController : Controller
+    {
+        // такой код для отрабатывания представлений
+        // Роут обязательно, View() передавать можно и название твоей вью и пустым оставить
+        // ну и модель как у тебя ниже в логин
+        [Route("Account/Login")]
+        /*public IActionResult Login()
+        {
+            return View("login");
+        }
+        
+        [HttpPost]*/
+        public async Task<IActionResult> Login(LogViewModel model, string returnUrl)
+        {
+            if (ModelState.IsValid)
+            {
+                if (ValidateUser(model.UserName, model.Password))
+                {
+                    var claims = new List<Claim>{
+                        new Claim(ClaimTypes.Name, model.UserName),
+                    };
+
+                    var claimsIdentity = new ClaimsIdentity(claims, CookieAuthenticationDefaults.AuthenticationScheme);
+                    await HttpContext.SignInAsync(
+                            CookieAuthenticationDefaults.AuthenticationScheme,
+                            new ClaimsPrincipal(claimsIdentity));
+                    //.SetAuthCookie(model.UserName, model.RememberMe);
+                    if (Url.IsLocalUrl(returnUrl))
+                    {
+                        return Redirect(returnUrl);
+                    }
+                    else
+                    {
+                        return RedirectToAction("Index", "Request");
+                    }
+                }
+                else
+                {
+                    ModelState.AddModelError("", "Неправильный пароль или логин");
+                }
+            }
+            return View(model);
+        }
+        public async Task<IActionResult> LogOff()
+        {
+            await HttpContext.SignOutAsync();
+
+            return RedirectToAction("Login", "Account");
+        }
+
+        private bool ValidateUser(string login, string password)
+        {
+            bool isValid = false;
+
+            using (HelpdeskContext _db = new HelpdeskContext())
+            {
+                try
+                {
+                    User user = (from u in _db.Users
+                                 where u.Login == login && u.Password == password
+                                 select u).FirstOrDefault();
+
+                    if (user != null)
+                    {
+                        isValid = true;
+                    }
+                }
+                catch
+                {
+                    isValid = false;
+                }
+            }
+            return isValid;
+        }
+    }
+}
+
---
+++ Controllers/UserController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+//using System.Data.Entity;
+using System.Data;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Microsoft.EntityFrameworkCore;
+using HelpDeskTrain.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HelpDeskTrain.Controllers
+{
+    [Authorize(Roles = "Администратор, Модератор, Исполнитель")]
+    public class UserController : Controller
+    {
+        private HelpdeskContext db = new HelpdeskContext();
+       [Route("User/Index")]
+        /*public IActionResult Index()
+       {
+           return View("Index");
+       }
+
+       */
+        [HttpGet]
+        public IActionResult Index()
+        {
+            var users = db.Users.Include(u => u.Department).Include(u => u.Role).ToList();
+            return View(users);
+        }
+    }
+}
+
